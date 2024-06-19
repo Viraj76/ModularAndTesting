006fbe2ce32a18d621984e05d23e32418562e066
Viraj76 <virajg.7680@gmail.com>
2024-06-19 08:50:52 +0530

    toolbar

--- hero/heroList/src/main/java/com/appsv/herolist/ui/HeroListScreen.kt
+++ hero/heroList/src/main/java/com/appsv/herolist/ui/HeroListScreen.kt
@@ -2,19 +2,25 @@ package com.appsv.herolist.ui
 
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.appsv.core.domain.ProgressBarState
 import com.appsv.herolist.components.HeroListItem
+import com.appsv.herolist.components.HeroListToolbar
 
-@OptIn(ExperimentalAnimationApi::class)
+@OptIn(ExperimentalAnimationApi::class, ExperimentalComposeUiApi::class)
 @Composable
 fun HeroListScreen(
     state : HeroListState,
@@ -22,16 +28,36 @@ fun HeroListScreen(
 ){
 
     Box(
-        modifier = Modifier.fillMaxSize().padding(20.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
 
     ){
-        LazyColumn(
+        Column {
+            val name = remember {
+                mutableStateOf("")
+            }
+            HeroListToolbar(
+                heroName = name.value,
+                onHeroNameChanged = {heroName->
+                                    name.value = heroName
+                },
+                onExecuteSearch = {
 
-        ) {
-            items(state.heros) { hero ->
-                HeroListItem(hero = hero, onSelectHero = {heroId ->
-                    navigateToHeroDetailScreen(heroId)
-                })
+                },
+                onShowFilterDialog = {
+
+                }
+            )
+
+            LazyColumn(
+
+            ) {
+                items(state.heros) { hero ->
+                    HeroListItem(hero = hero, onSelectHero = {heroId ->
+                        navigateToHeroDetailScreen(heroId)
+                    })
+                }
             }
         }
         if (state.progressBarState is ProgressBarState.Loading) {
